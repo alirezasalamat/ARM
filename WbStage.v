0e690e797c763ce47086a511289128bddc4dec4d
aminb7 <45150807+aminb7@users.noreply.github.com>
2021-05-11 17:12:51 +0430

    Add WB stage.

--- WbStage.v
+++ WbStage.v
@@ -1,5 +1,10 @@
 `include "defines.v"
 
-module WbStage(input clk, input rst);
-  
+module WbStage(alu_result, mem_result, mem_read, out);
+    input [31:0]alu_result, mem_result;
+    input mem_read;
+
+    output [31:0]out;
+
+    Mux32_2 Mux32_2_0(alu_result, mem_result, mem_read, out);
 endmodule
