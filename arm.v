a90d5d95e4645a42ccd0ed8992c4067d223535d5
aminb7 <45150807+aminb7@users.noreply.github.com>
2021-05-11 17:38:39 +0430

    Add hazard unit and status reg in arm.

--- arm.v
+++ arm.v
@@ -1,13 +1,15 @@
 `include "defines.v"
 
 module Arm(input clk, input rst);
-  IfStage IfStage_0(clk, rst);
-  IfReg IfReg_0(clk, rst);
-  IdStage IdStage_0(clk, rst);
-  IdReg IdReg_0(clk, rst);
-  ExeStage ExeStage_0(clk, rst);
-  ExeReg ExeReg_0(clk, rst);
-  MemStage MemStage_0(clk, rst);
-  MemReg MemReg_0(clk, rst);
-  WbStage WbStage_0(clk, rst);
+    IfStage IfStage_0(clk, rst);
+    IfReg IfReg_0(clk, rst);
+    IdStage IdStage_0(clk, rst);
+    IdReg IdReg_0(clk, rst);
+    ExeStage ExeStage_0(clk, rst);
+    ExeReg ExeReg_0(clk, rst);
+    MemStage MemStage_0(clk, rst);
+    MemReg MemReg_0(clk, rst);
+    WbStage WbStage_0(clk, rst);
+    StatusRegister StatusRegister_0();
+    HazardDetectionUnit HazardDetectionUnit_0();
 endmodule
