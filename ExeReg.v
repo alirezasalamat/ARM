a92e047383102c37241b686809eaf39e9650f338
Alireza Salamat <52575958+alirezasalamat@users.noreply.github.com>
2021-05-11 20:48:01 +0430

    st_val changed ro val_Rm

--- ExeReg.v
+++ ExeReg.v
@@ -1,13 +1,13 @@
 `include "defines.v"
 
-module ExeReg(clk, rst, wb_en_in, mem_read_in, mem_write_in, alu_result_in, st_val_in, dest_in
-        , wb_en, mem_read, mem_write, alu_result, st_val, dest);
+module ExeReg(clk, rst, wb_en_in, mem_read_in, mem_write_in, alu_result_in, val_Rm_in, dest_in
+        , wb_en, mem_read, mem_write, alu_result, val_Rm_out dest);
     input clk, rst, wb_en_in, mem_read_in, mem_write_in;
-    input [31:0]alu_result_in, st_val_in;
+    input [31:0]alu_result_in, val_Rm_in;
     input [3:0]dest_in;
 
     output reg wb_en, mem_read, mem_write;
-    output reg [31:0]alu_result, st_val;
+    output reg [31:0]alu_result, val_Rm_out;
     output reg [3:0]dest;
 
     always @(posedge clk, posedge rst) begin
@@ -16,7 +16,7 @@ module ExeReg(clk, rst, wb_en_in, mem_read_in, mem_write_in, alu_result_in, st_v
             mem_read <= 0;
             mem_write <= 0;
             alu_result <= 0;
-            st_val <= 0;
+            val_Rm_out <= 0;
             dest <= 0;
         end
         else begin
@@ -24,7 +24,7 @@ module ExeReg(clk, rst, wb_en_in, mem_read_in, mem_write_in, alu_result_in, st_v
             mem_read <= mem_read_in;
             mem_write <= mem_write_in;
             alu_result <= alu_result_in;
-            st_val <= st_val_in;
+            val_Rm_out <= val_Rm_in;    
             dest <= dest_in;
         end
     end
