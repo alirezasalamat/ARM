48f19dc5d813003303c77eba666925f7abad3476
aminb7 <45150807+aminb7@users.noreply.github.com>
2021-06-25 23:20:40 +0430

    Refactoring.

--- CacheController.v
+++ CacheController.v
@@ -7,7 +7,8 @@ module CacheController(clk, rst, address, wdata, MEM_R_EN, MEM_W_EN, sram_rdata,
     input [63:0] sram_rdata;
 
     output ready, write, read;
-    output [31:0] rdata, sram_address, sram_wdata;
+	output [31:0] rdata;
+	output reg [31:0] sram_wdata, sram_address;
 
     wire[16:0] cache_address;
 	wire[31:0] temp_address;
@@ -59,11 +60,14 @@ module CacheController(clk, rst, address, wdata, MEM_R_EN, MEM_W_EN, sram_rdata,
 					):
 					32'bz;
 
-	assign sram_address = (ps == READ) ? address :
-						(ps == WRITE) ? address :
-						32'bz;
+	always @(ps, address) begin
+		sram_address = (ps == READ) ? address :
+						(ps == WRITE) ? address : 32'bz;
+	end
 
-	assign sram_wdata = (ps == WRITE) ? wdata : 32'bz;
+	always @(ps, wdata) begin
+		sram_wdata = (ps == WRITE) ? wdata : 32'bz;
+	end
 
 	assign write = (ps == WRITE);
 	assign read = (ps == READ);
